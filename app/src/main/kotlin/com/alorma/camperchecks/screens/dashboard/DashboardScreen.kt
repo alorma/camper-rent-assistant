7896070494d80a54513cc7ec2eec2b4fcc474200
Bernat Borrás <bernat.borras@adevinta.com>
2026-03-13 17:51:53 +0100

    Cleanup

--- app/src/main/kotlin/com/alorma/camperchecks/screens/dashboard/DashboardScreen.kt
+++ app/src/main/kotlin/com/alorma/camperchecks/screens/dashboard/DashboardScreen.kt
@@ -1,7 +1,10 @@
 package com.alorma.camperchecks.screens.dashboard
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.alorma.camperchecks.ui.components.loading.FullscreenLoading
 import com.alorma.camperchecks.ui.components.scaffold.AppScaffold
 import com.alorma.camperchecks.ui.components.topbar.StyledTopAppBar
 import com.alorma.camperchecks.ui.responsive.ResponsiveCenteredContainer
@@ -14,9 +17,11 @@ fun DashboardScreen() {
         title = { Text(text = "Dasboard") },
       )
     },
-  ) {
-    ResponsiveCenteredContainer {
-      Text(text = "Dasboard")
+  ) { paddingValues ->
+    ResponsiveCenteredContainer(
+      modifier = Modifier.padding(paddingValues),
+    ) {
+      FullscreenLoading()
     }
   }
 }
