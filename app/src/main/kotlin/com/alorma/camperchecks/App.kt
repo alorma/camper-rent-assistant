b6e5de0c6fe7b26e2d6d4d0807645c6abd0ba901
Bernat Borrás <bernat.borras@adevinta.com>
2026-03-13 17:37:10 +0100

    Update gradle wrapper

--- app/src/main/kotlin/com/alorma/camperchecks/App.kt
+++ app/src/main/kotlin/com/alorma/camperchecks/App.kt
@@ -3,10 +3,17 @@ package com.alorma.camperchecks
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.alorma.camperchecks.ui.components.scaffold.AppScaffold
+import com.alorma.camperchecks.ui.components.topbar.StyledTopAppBar
 
 @Composable
 fun App() {
-  AppScaffold {
+  AppScaffold(
+    topBar = {
+      StyledTopAppBar(
+        title = { Text(text = "Dashboard") },
+      )
+    },
+  ) {
     Text(text = "Hello world")
   }
 }
